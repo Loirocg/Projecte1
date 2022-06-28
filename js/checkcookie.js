ebc6c3a5fd45320d7f870e2d566a39598ac830da
blum <alfredoblumtorres@gmail.com>
2022-06-28 12:25:35 +0200

    Testing cookie

---
+++ js/checkcookie.js
@@ -0,0 +1,13 @@
+window.onload = function (){
+    checkCookie();
+}
+
+function checkCookie(){
+    if (document.cookie == ""){
+        window.location.href = "login.html";
+    } else if (document.cookie =! "") {
+        window.location.href = "apuntes.html";
+    } else{
+        alert("Error");
+    }
+}
